1f99de25412ff2bd7e311952fad88c666b205944
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-23 15:50:13 +0300

    create class of compare NameComparor

---
+++ task 4/NameComparor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    internal class NameComparor : IComparer<Person>
+    {
+        public int Compare(Person firstPerson, Person secondPerson)
+        {
+            int result = firstPerson.Name.Length.CompareTo(secondPerson.Name.Length);
+            
+            if (result == 0) result = string.Compare(firstPerson.Name.ToLower()[0].ToString(), secondPerson.Name.ToLower()[0].ToString());
+
+            return result;
+        }
+    }
+}
--- task 4/Person.cs
+++ task 4/Person.cs
@@ -8,7 +8,7 @@ namespace task_4
 {
     internal class Person
     {
-        private string Name { get; set; }
+        public string Name { get; set; }
         public int Age { get; set; }
 
         public Person(string name, int age)
