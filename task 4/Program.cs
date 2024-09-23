b3aa5999c74a88c2e99944811823e17a6bb057aa
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-23 15:58:26 +0300

    done with this task

--- task 4/Program.cs
+++ task 4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using task_4;
 
 class Task
 {
@@ -6,7 +7,38 @@ class Task
     {
         Console.Write("Enter amount of persons = ");
         int size = int.Parse(Console.ReadLine());
+        Line();
 
+        List<Person> nameComparor = new List<Person>();
+        List<Person> ageComparor = new List<Person>();
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Enter {i + 1} person = ");
+            string[] enteredInformation = Console.ReadLine().Split();
+
+            string name = enteredInformation[0];
+            int age = int.Parse(enteredInformation[1]);
+            Person person = new Person(name, age);
+
+            nameComparor.Add(person);
+            ageComparor.Add(person);
+            Line();
+        }
+
+        nameComparor.Sort(new NameComparor());
+        ageComparor.Sort(new AgeComparor());
+
+        for (int i = 0; i < nameComparor.Count; i++)
+        {
+            Console.WriteLine(nameComparor[i]);
+        }
+
+        Line();
+        for (int i = 0; i < ageComparor.Count; i++)
+        {
+            Console.WriteLine(ageComparor[i]);
+        }
     }
     public static void Line()
     {
