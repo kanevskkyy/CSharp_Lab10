57878bafe499e8a6da97fa8bb133eee1f87bd030
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-23 14:46:24 +0300

    create class Person and method ComppareTo

---
+++ task 3/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    internal class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            int nameComparison = Name.CompareTo(other.Name);
+            if (nameComparison != 0)
+            {
+                return nameComparison;
+            }
+
+            int ageComparison = this.Age.CompareTo(other.Age);
+            if (ageComparison != 0)
+            {
+                return ageComparison;
+            }
+
+            return City.CompareTo(other.City);
+        }
+    }
+}
