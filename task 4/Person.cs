9f8c2bc5932b5305787b148b987e095fe85e677e
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-23 15:44:25 +0300

    create base class

---
+++ task 4/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    internal class Person
+    {
+        private string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString() 
+        {
+            return $"Person : {Name} {Age}";
+        }
+    }
+}
