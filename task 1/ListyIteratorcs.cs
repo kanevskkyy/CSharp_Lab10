d80614054be934d3c2601603344d36721346f3dc
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-23 14:20:50 +0300

    виніс клас в окремий файл

---
+++ task 1/ListyIteratorcs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    internal class ListyIterator<T>
+    {
+        private List<T> elements;
+        private int index;
+        public ListyIterator()
+        {
+             elements = new List<T>();
+             index = 0;
+        }
+
+        public void AddElement(T element)
+        {
+            elements.Add(element);
+        }
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+             return index < elements.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (elements.Count == 0)
+            {
+                 throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(elements[index]);
+        }
+    }
+}
--- task 1/Program.cs
+++ task 1/Program.cs
@@ -1,46 +1,6 @@
 ﻿using System;
+using task_1;
 using System.Collections.Generic;
-
-class ListyIterator<T>
-{
-    private List<T> elements;
-    private int index;
-
-    public ListyIterator()
-    {
-        elements = new List<T>();
-        index = 0;
-    }
-
-    public void AddElement(T element)
-    {
-        elements.Add(element);
-    }
-
-    public bool Move()
-    {
-        if (HasNext())
-        {
-            index++;
-            return true;
-        }
-        return false;
-    }
-
-    public bool HasNext()
-    {
-        return index < elements.Count - 1;
-    }
-
-    public void Print()
-    {
-        if (elements.Count == 0)
-        {
-            throw new InvalidOperationException("Invalid Operation!");
-        }
-        Console.WriteLine(elements[index]);
-    }
-}
 
 class Task
 {
