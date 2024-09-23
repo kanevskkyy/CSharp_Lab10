fc176b9e3e02bc7ffc1d667ce425e32d768a1d3a
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-23 14:27:46 +0300

    start doing task 2, create class

---
+++ task 2/ListyIterator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    internal class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+        private int index;
+        public ListyIterator()
+        {
+            elements = new List<T>();
+            index = 0;
+        }
+
+        public void AddElement(T element)
+        {
+            elements.Add(element);
+        }
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return index < elements.Count - 1;
+        }
+        public void Print()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(elements[index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                yield return elements[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+}
