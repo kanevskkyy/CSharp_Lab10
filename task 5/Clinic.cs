fcf4f753e86d5f7e213155fd358fdf882d6f5f27
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-23 20:05:46 +0300

    create function AddPet

---
+++ task 5/Clinic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_5
+{
+    internal class Clinic
+    {
+        public string ClinicName { get; set; }
+        private List<Pet> Rooms;
+        private int centerRoom;
+
+        public Clinic(string clinicName, int roomAmount)
+        {
+            ClinicName = clinicName;
+            if (roomAmount % 2 == 0) throw new ArgumentException("Rooms amount, cannot be even");
+            else 
+            {
+                centerRoom = roomAmount / 2;
+                Rooms = new List<Pet>(new Pet[roomAmount]);
+            }
+        }
+
+        public bool AddPet(Pet pet)
+        {
+            if (Rooms[centerRoom] == null)
+            {
+                Rooms[centerRoom] = pet;
+                return true;
+            }
+            if (Rooms[centerRoom - 1] == null) 
+            {
+                Rooms[centerRoom - 1] = pet;
+                return true;
+            }
+            if (Rooms[centerRoom + 1] == null)
+            { 
+                Rooms[centerRoom + 1] = pet;
+                return true;
+            }
+
+            for(int i = 0; i < centerRoom - 1; i++)
+            {
+                if (Rooms[i] == null)
+                {
+                    Rooms[i] = pet;
+                    return true;
+                }
+            }
+
+            for(int i = centerRoom + 1; i < Rooms.Count; i++)
+            {
+                if (Rooms[i] == null)
+                {
+                    Rooms[i] = pet;
+                    return true;
+                }
+            }
+            
+            return false; 
+        }
+    }
+}
--- task 5/Pet.cs
+++ task 5/Pet.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +18,11 @@ namespace task_5
             Name = name;
             Age = age;
             Kind = kind;
+        }
+
+        public override string ToString()
+        {
+            return $"Name : {Name}, Age : {Age}, Kind : {Kind}";
         }
     }
 }
