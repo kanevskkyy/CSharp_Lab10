b4048127530d54451cc43384aeba25916ef87da3
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-23 15:53:12 +0300

    create class AgeComparor and function of compare

--- task 4/Program.cs
+++ task 4/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+class Task
+{
+    static void Main()
+    {
+        Console.Write("Enter amount of persons = ");
+        int size = int.Parse(Console.ReadLine());
+
+    }
+    public static void Line()
+    {
+        Console.WriteLine("=======================================");
+    }
+}
