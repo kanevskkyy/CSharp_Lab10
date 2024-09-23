d7fe6f52546af83095bb373c88541edfcf400f60
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-23 13:55:35 +0300

    done with realisation of class and all functions

--- task 1/Program.cs
+++ task 1/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using System;
+
+class ListyIterator<T>
+{
+    private List<T> elements = new List<T>();
+    private int index = -1;
+
+    public bool Move()
+    {
+        if (this.HasNext())
+        {
+            index++;
+            return true;
+        }
+        else return false;
+    }
+
+    public bool HasNext() 
+    {
+        return index < elements.Count;
+    }
+
+    public void Print()
+    {
+        if (elements.Count == 0) throw new InvalidOperationException("Invalid Operation!");
+        if (index < 0 || index >= elements.Count)  throw new InvalidOperationException("Invalid Operation!");
+
+        Console.WriteLine(elements[index]);
+    }
+}
+
+
+class Task
+{
+    static void Main()
+    {
+
+    }
+    public static void Line()
+    {
+        Console.WriteLine("=======================================");
+    }
+}
